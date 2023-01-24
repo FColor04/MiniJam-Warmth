00ca55c519f51e6fb779c0a85b73dc33e9b0b169
XenoLoaf <52606003+XenoLoaf@users.noreply.github.com>
2023-01-24 19:39:34 +0800

    Update ObscurusDebugger.cs

--- ObscurusDebugger.cs
+++ ObscurusDebugger.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualBasic;
 using System.Diagnostics;
 
 
@@ -6,11 +7,19 @@ namespace ObscurusDebuggerTools {
     public class ObscurusDebugger {
 
 
-        public bool DebugMode;
+        public bool DebugMode = false;
+        public readonly int DebugInt = 5;
+        public readonly float DebugFloat = 3.141592653589793238462643383279f;
 
+        #region ~Log~
         public static void Log(string message)
         {
             Debug.WriteLine(message);
+        }
+
+        public static void Log(float Number)
+        {
+            Debug.WriteLine("Log: " + Number.ToString()); 
         }
 
         public static void LogWarning(string message)
@@ -22,5 +31,16 @@ namespace ObscurusDebuggerTools {
         {
             Debug.WriteLine("[ERROR] " + message);
         }
+
+        public static void ThrowError()
+        {
+            Debug.WriteLine("An Error Has Occurred...");
+        }
+        #endregion
+
+        #region ~Tools~
+
+        #endregion
+
     }
 }
