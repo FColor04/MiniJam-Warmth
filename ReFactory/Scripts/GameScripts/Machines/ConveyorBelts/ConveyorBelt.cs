2018bba69f54bbe1b1ac431b2fda069dab1ead79
XenoLoaf <52606003+XenoLoaf@users.noreply.github.com>
2023-02-05 10:56:31 +0800

    Update

--- ReFactory/Scripts/GameScripts/Machines/ConveyorBelts/ConveyorBelt.cs
+++ ReFactory/Scripts/GameScripts/Machines/ConveyorBelts/ConveyorBelt.cs
@@ -6,23 +6,34 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MainGameFramework;
 using NLua;
+using NLog;
 using ReFactory.UISystem;
 using Debug = ReFactory.Debugger.Debug;
 
 namespace ReFactory.GameScripts.Machines.ConveyorBelts
 {
 
+    #region Class Opening & Variables
     public class ConveyorBelt : GridEntity
     {
-        // private Lua luaBelt = new Lua();
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public static List<ConveyorBelt> ConveyorBelts = new();
-        public override float DestroyTime => 0.1f; // Override GridEntity DestroyTime.
+        private static Stack<ConveyorBelt> _updateStack = new();
+
+        private bool beltSet = false;
+
+        public override float DestroyTime => 0.1f;
         private Func<Texture2D> _getSprite = () => GameContent.StraightConveyorBelt.GetSprite();
+        public override Texture2D sprite => _getSprite();
+        public override HashSet<Point> OccupiedRelativePoints => new() { new Point(0, 0) };
+        public override Func<bool> OnDestroy => () => Inventory.AddItem(new Item("Belt", 1));
+        private bool _updatedThisFrame;
+        private List<BeltEntity> _leftSideBeltEntities = new();
+        private List<BeltEntity> _rightSideBeltEntities = new();
+        #endregion
 
-        public override HashSet<Point> OccupiedRelativePoints => new() { new Point(0, 0) }; // Override GridEntity OccupiedRelativePoints.
-        public override Func<bool> OnDestroy => () => Inventory.AddItem(new Item("Belt", 1)); // Override GridEntity OnDestroy.
-        public List<Entity> BeltEntities = new(); // List of entities on this Belt
-
+        #region Conveyor Belt Constructor & Disposal Method
         public ConveyorBelt()
         {
             ConveyorBelts.Add(this);
@@ -37,19 +48,14 @@ namespace ReFactory.GameScripts.Machines.ConveyorBelts
             base.Dispose(true);
         }
 
-        private void OnGridBuild(Point entityPosition, GridEntity entity)
-        {
-            if (entity is ConveyorBelt && Math.Abs(position.X - entityPosition.X) == World.GridSize || Math.Abs(position.Y - entityPosition.Y) == World.GridSize)
-            {
-                UpdateSprite();
-            }
-        }
+        #endregion
+
+
 
         #region Global Update Method
         public static void GlobalUpdate(float deltaTime)
         {
-            if(Keys.P.WasPressedThisFrame())
-            { Debug.Log("new entity");
+            if (Keys.P.WasPressedThisFrame() && ConveyorBelts.Any())
                 ConveyorBelts.First().PlaceNewEntity(new BeltEntity()
                 {
                     entity = new Entity()
@@ -58,20 +64,117 @@ namespace ReFactory.GameScripts.Machines.ConveyorBelts
                         position = ConveyorBelts.First().position
                     }
                 }, false);
-                
+
+            RefreshUpdateStack();
+
+            while (_updateStack.TryPop(out var belt))
+            {
+                ConveyorBelt targetBelt = belt.GetTargetBelt();
+                if (targetBelt != null)
+                {
+                    float rotationDiff = targetBelt.rotation - belt.rotation;
+                    //Debug.Log(rotationDiff + "_FirstCheck");
+                    while (rotationDiff < 0)
+                        rotationDiff += 360;
+                    rotationDiff %= 360;
+                    bool targetOtherSide = rotationDiff >= 180;
+
+
+                    if (belt._leftSideBeltEntities.Count! >= 0 && belt._rightSideBeltEntities.Count! >= 0)
+                    {
+                        continue;
+                    }
+
+                    if (targetBelt.HasSpaceForNewEntity(targetOtherSide))
+                    {
+                        float previousProgress = 2;
+                        foreach (var beltEntity in belt._rightSideBeltEntities.OrderByDescending(beltE => beltE.progress))
+                        {
+                            if (!beltEntity.Progress(deltaTime, ref previousProgress)) break;
+                            belt._rightSideBeltEntities.Remove(beltEntity);
+                            targetBelt.PlaceNewEntity(beltEntity, targetOtherSide);
+                        }
+                    }
+                    //else
+                        //Debug.Log("No space for me A");
+
+                    if (targetBelt.HasSpaceForNewEntity(!targetOtherSide))
+                    {
+                        var previousProgress = 1 + ItemSpacing;
+                        foreach (var beltEntity in belt._leftSideBeltEntities.OrderByDescending(beltE => beltE.progress))
+                        {
+                            if (!beltEntity.Progress(deltaTime, ref previousProgress)) break;
+                            belt._leftSideBeltEntities.Remove(beltEntity);
+                            targetBelt.PlaceNewEntity(beltEntity, !targetOtherSide);
+                        }
+                    }
+                    //else
+                        //Debug.Log("No space for me B");
+                }
+                //else
+                    //Debug.Log($"No space for me C {belt.rotation} {belt.position}");
             }
         }
         #endregion
 
+        #region Refresh Stack Method
+        private static void RefreshUpdateStack()
+        {
+            _updateStack.Clear();
+            for (int i = 0; i < ConveyorBelts.Count; i++)
+            {
+                if (_updateStack.Contains(ConveyorBelts[i]))
+                    continue;
+
+                _updateStack.Push(ConveyorBelts[i]);
+                ConveyorBelt lastCheckedBelt = ConveyorBelts[i];
+                ConveyorBelt sourceBelt = ConveyorBelts[i].GetSourceBelt();
+
+                while (sourceBelt != null && sourceBelt != lastCheckedBelt && sourceBelt != ConveyorBelts[i])
+                {
+                    lastCheckedBelt = sourceBelt;
+                    sourceBelt = sourceBelt.GetSourceBelt();
+                }
+
+                while (lastCheckedBelt != null)
+                {
+                    if (!_updateStack.Contains(lastCheckedBelt))
+                    {
+                        _updateStack.Push(lastCheckedBelt);
+                    }
+                    else
+                        break;
+                    lastCheckedBelt = lastCheckedBelt.GetTargetBelt();
+                }
+            }
+        }
+        #endregion
+
+        #region Place New Entity Method (Belt)
         private void PlaceNewEntity(BeltEntity beltEntity, bool left)
         {
             beltEntity.pointA = position + -rotation.RotationToVector2() * World.GridSize;
             beltEntity.pointB = position;
         }
+        #endregion
+
+        #region Has Space Method
+        private bool HasSpaceForNewEntity(bool left)
+        {
+            if (left)
+            {
+                return _leftSideBeltEntities.Count == 0 || _leftSideBeltEntities.MinBy(beltE => beltE.progress).progress > ItemSpacing;
+            }
+            return _rightSideBeltEntities.Count == 0 || _rightSideBeltEntities.MinBy(beltE => beltE.progress).progress > ItemSpacing;
+        }
+        private const float ItemSpacing = 1 / 4f;
+
+        #endregion
 
         #region Get Target Belt
         public ConveyorBelt GetTargetBelt()
         {
+
             Point beltTarget = position.ToPoint();
             switch (rotation)
             {
@@ -123,29 +226,106 @@ namespace ReFactory.GameScripts.Machines.ConveyorBelts
 
         #endregion
 
+        #region On Grid Build Method
+        private void OnGridBuild(Point entityPosition, GridEntity entity)
+        {
+            if (entity is ConveyorBelt belt &&
+                // ReSharper disable once CompareOfFloatsByEqualityOperator
+                Math.Abs(position.X - entityPosition.X) == World.GridSize &&
+                // ReSharper disable once CompareOfFloatsByEqualityOperator
+                Math.Abs(position.Y - entityPosition.Y) == World.GridSize
+                )
+            {
+                //UpdateSprite();
+            }
+        }
+        #endregion
+
         #region Update Sprite Method
         public void UpdateSprite()
         {
             Point clockwisePoint = position.ToPoint();
             Point counterClockwisePoint = position.ToPoint();
-            
-            _getSprite = () => GameContent.StraightConveyorBelt.GetSprite();
+            switch (rotation)
+            {
+                case 0: //Top
+                    clockwisePoint += new Point(World.GridSize, 0);
+                    counterClockwisePoint += new Point(-World.GridSize, 0);
+                    break;
+                case 90: //Right
+                    clockwisePoint += new Point(0, World.GridSize);
+                    counterClockwisePoint += new Point(0, -World.GridSize);
+                    break;
+                case 180: //Bottom
+                    clockwisePoint += new Point(-World.GridSize, 0);
+                    counterClockwisePoint += new Point(World.GridSize, 0);
+                    break;
+                case 270: //Left
+                    clockwisePoint += new Point(0, -World.GridSize);
+                    counterClockwisePoint += new Point(0, World.GridSize);
+                    break;
+            }
+
+            bool clockwiseValid = MainGame.Instance.World.gridElements.TryGetValue(clockwisePoint, out var cwEntity)
+                                && cwEntity is ConveyorBelt clockwiseBelt &&
+                                clockwiseBelt.rotation == (rotation + 270) % 360;
+
+            bool counterClockwiseValid = MainGame.Instance.World.gridElements.TryGetValue(counterClockwisePoint, out var ccwEntity)
+                                && ccwEntity is ConveyorBelt counterClockwiseBelt &&
+                                counterClockwiseBelt.rotation == (rotation + 90) % 360;
+            if (!clockwiseValid && !counterClockwiseValid || clockwiseValid && counterClockwiseValid)
+            {
+                _getSprite = () => GameContent.StraightConveyorBelt.GetSprite();
+                return;
+            }
+
+            _getSprite = clockwiseValid ? () => GameContent.ClockwiseConveyorBelt.GetSprite() : () => GameContent.CounterClockwiseConveyorBelt.GetSprite();
         }
         #endregion
 
+        #region Belt Entity
         public struct BeltEntity
         {
-            enum BeltDirection
-            {
-                N_S, S_N, W_E, E_W, N_W, W_S, S_E, E_N, N_E, E_S, S_W, W_N
-            }
-            
             public Vector2 pointA;
             public Vector2 pointB;
+            public float progress;
             public Entity entity;
+
+            public bool Progress(float deltaTime, ref float previousProgress)
+            {
+                if (progress + deltaTime >= previousProgress - ItemSpacing) return false;
+
+                progress += deltaTime;
+                entity.position = Vector2.Lerp(pointA, pointB, progress);
+                previousProgress = progress;
+                return progress >= 1;
+            }
         }
-
-
-
     }
 }
+#endregion
+
+#region Issues & Improvements to Address
+/*===========================================================================
+ * = ISSUES = *
+ * = 
+ * = 
+ * = 
+ * = 
+ * = 
+ * = 
+ * = 
+ * = 
+ * = 
+ * = IMPROVEMENTS = *
+ * = 
+ * = 
+ * = 
+ * = 
+ * = 
+ * = 
+ * = 
+ * = 
+ * = 
+ ============================================================================*/
+#endregion
---
+++ ReFactory/Scripts/Utility/ExtraMathFunctions.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace ReFactory.Utility
+{
+    public class ExtraMathFunctions
+    {
+        public static T Clamp<T>(T value, T min, T max) where T : IComparable<T>
+        {
+            if (value.CompareTo(min) < 0) return min;
+            if (value.CompareTo(max) > 0) return max;
+            return value;
+        }
+        public static float InverseLerp(float a, float b, float value)
+        {
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (a == b) return 0.0f;
+            return (value - a) / (b - a);
+        }
+
+        public static float InverseLerpClamped(float a, float b, float value)
+        {
+            if (a == b) return 0.0f;
+            float result = (value - a) / (b - a);
+            return Clamp(result, 0.0f, 1.0f);
+        }
+
+        #region Simplex Noise
+        public class SimplexNoise
+        {
+            private const int GradientSizeTable = 256;
+            private readonly float[] _gradients = new float[GradientSizeTable * 3];
+            private readonly int[] _perm = new int[GradientSizeTable];
+
+            public SimplexNoise(int seed)
+            {
+                Random rnd = new Random(seed);
+
+                for (int i = 0; i < GradientSizeTable; i++)
+                {
+                    float z = 1f - 2f * (float)rnd.NextDouble();
+                    float r = (float)Math.Sqrt(1f - z * z);
+                    float theta = 2 * (float)Math.PI * (float)rnd.NextDouble();
+                    _gradients[i * 3] = r * (float)Math.Cos(theta);
+                    _gradients[i * 3 + 1] = r * (float)Math.Sin(theta);
+                    _gradients[i * 3 + 2] = z;
+                }
+
+                for (int i = 0; i < GradientSizeTable; i++)
+                {
+                    _perm[i] = i;
+                }
+
+                for (int i = 0; i < GradientSizeTable; i++)
+                {
+                    int j = rnd.Next(GradientSizeTable);
+                    int k = _perm[i];
+                    _perm[i] = _perm[j];
+                    _perm[j] = k;
+                }
+            }
+
+            public float Noise(float x, float y, float z)
+            {
+                float n0, n1, n2, n3;
+                float s = (x + y + z) / 3;
+                int i = FastFloor(x + s);
+                int j = FastFloor(y + s);
+                int k = FastFloor(z + s);
+
+                float t = (i + j + k) / 6f;
+                float X0 = i - t;
+                float Y0 = j - t;
+                float Z0 = k - t;
+
+                float x0 = x - X0;
+                float y0 = y - Y0;
+                float z0 = z - Z0;
+
+                int i1, j1, k1;
+                int i2, j2, k2;
+
+                if (x0 >= y0)
+                {
+                    if (y0 >= z0)
+                    {
+                        i1 = 1; j1 = 0; k1 = 0; i2 = 1; j2 = 1; k2 = 0;
+                    }
+                    else if (x0 >= z0)
+                    {
+                        i1 = 1; j1 = 0; k1 = 0; i2 = 1; j2 = 0; k2 = 1;
+                    }
+                    else
+                    {
+                        i1 = 0; j1 = 0; k1 = 1; i2 = 1; j2 = 0; k2 = 1;
+                    }
+                }
+                else
+                {
+                    if (y0 < z0)
+                    {
+                        i1 = 0; j1 = 0; k1 = 1; i2 = 0; j2 = 1; k2 = 1;
+                    }
+                    else if (x0 < z0)
+                    {
+                        i1 = 0; j1 = 1; k1 = 0; i2 = 0; j2 = 1; k2 = 1;
+                    }
+                    else
+                    {
+                        i1 = 0; j1 = 1; k1 = 0; i2 = 1; j2 = 1; k2 = 0;
+                    }
+                }
+
+                float x1 = x0 - i1 + (1f / 6f);
+                float y1 = y0 - j1 + (1f / 6f);
+                float z1 = z0 - k1 + (1f / 6f);
+                float x2 = x0 - i2 + (2f / 6f);
+                float y2 = y0 - j2 + (2f / 6f);
+                float z2 = z0 - k2 + (2f / 6f);
+                float x3 = x0 - 1f + (3f / 6f);
+                float y3 = y0 - 1f + (3f / 6f);
+                float z3 = z0 - 1f + (3f / 6f);
+
+                int ii = i & 0xff;
+                int jj = j & 0xff;
+                int kk = k & 0xff;
+
+                float t0 = 0.6f - x0 * x0 - y0 * y0 - z0 * z0;
+                if (t0 < 0) n0 = 0.0f;
+                else
+                {
+                    int gi0 = _perm[ii + _perm[jj + _perm[kk]]] % 12;
+                    t0 *= t0;
+                    n0 = t0 * t0 * Dot(_gradients, gi0, x0, y0, z0);
+                }
+
+                float t1 = 0.6f - x1 * x1 - y1 * y1 - z1 * z1;
+                if (t1 < 0) n1 = 0.0f;
+                else
+                {
+                    int gi1 = _perm[ii + i1 + _perm[jj + j1 + _perm[kk + k1]]] % 12;
+                    t1 *= t1;
+                    n1 = t1 * t1 * Dot(_gradients, gi1, x1, y1, z1);
+                }
+
+                float t2 = 0.6f - x2 * x2 - y2 * y2 - z2 * z2;
+                if (t2 < 0) n2 = 0.0f;
+                else
+                {
+                    int gi2 = _perm[ii + i2 + _perm[jj + j2 + _perm[kk + k2]]] % 12;
+                    t2 *= t2;
+                    n2 = t2 * t2 * Dot(_gradients, gi2, x2, y2, z2);
+                }
+
+                float t3 = 0.6f - x3 * x3 - y3 * y3 - z3 * z3;
+                if (t3 < 0) n3 = 0.0f;
+                else
+                {
+                    int gi3 = _perm[ii + 1 + _perm[jj + 1 + _perm[kk + 1]]] % 12;
+                    t3 *= t3;
+                    n3 = t3 * t3 * Dot(_gradients, gi3, x3, y3, z3);
+                }
+                return 32.0f * (n0 + n1 + n2 + n3);
+            }
+
+            private static float Dot(float[] g, int i, float x, float y, float z)
+            {
+                return g[i * 3] * x + g[i * 3 + 1] * y + g[i * 3 + 2] * z;
+            }
+
+            private static int FastFloor(float x)
+            {
+                int xi = (int)x;
+                return x < xi ? xi - 1 : xi;
+            }
+        }
+        #endregion
+
+
+
+
+//=========End of File=========//
+    }
+}
