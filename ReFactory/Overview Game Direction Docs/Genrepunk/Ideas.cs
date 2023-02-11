93c9005368492fc9519b255adb5343d9baa29c62
XenoLoaf <52606003+XenoLoaf@users.noreply.github.com>
2023-02-11 19:46:22 +0800

    Create Ideas.cs

---
+++ ReFactory/Overview Game Direction Docs/Genrepunk/Ideas.cs
@@ -0,0 +1,82 @@
+﻿/*
+
+In a Genrepunk-themed factory/automation game, players could expect to encounter a mix of technology and creatures inspired by each of the different punk genres. 
+
+Some possible tech and creatures that could exist include:
+
+Steampunk-Clockpunk inspired tech and creatues:
+
+Steam - powered robots and automatons
+Clockwork machines and contraptions
+Futuristic vehicles, Airships and dirigibles powered by steam or clockwork
+Mechanical horses, dragons and other beasts and steam-powered vehicles
+Clockwork-powered golems and automatons
+Automated guardian constructs and traps
+
+Dieselpunk-inspired tech:
+
+Diesel - powered robots and automatons
+Art deco-style factories and machinery
+Diesel-powered airships and war machines
+Futuristic vehicles powered by diesel engines
+
+Atompunk-inspired tech:
+
+Atomic - powered robots and automatons
+Nuclear reactors and energy-generating facilities
+Atomic-powered submarines and airships
+Futuristic weapons and gadgets powered by atomic energy
+
+Cyberpunk-inspired tech:
+
+Advanced computer systems and virtual reality interfaces
+Cyborgs and artificial intelligence entities
+Hacking tools and weapons
+Futuristic weapons and gadgets
+
+
+Biopunk-inspired tech:
+
+Biotechnology - powered robots and automatons
+Genetic engineering facilities and labs
+Synthetic creatures and genetically modified organisms
+Medical and biotech devices and tools
+
+Nanopunk-inspired creatures:
+
+Nanobot swarms and nanomachine constructs
+Futuristic weapons and gadgets powered by nanotechnology
+Synthetic creatures and biotechnology-powered organisms
+Futuristic vehicles powered by nanotechnology.
+
+In this game, players could create and manage their own factories, automating production processes and utilizing a mix of these advanced technologies to produce a variety of products and goods. 
+Players could also encounter and recruit various creatures and automatons, each with unique abilities and strengths, to aid in the production process.
+
+*/
+
+/*
+===Quest Ideas===
+
+Retrieve a lost artifact powered by clockwork technology that has been stolen by a group of dieselpunk airship pirates.
+
+Investigate and stop a series of cyberattacks on a solarpunk village caused by a rogue group of nanopunk hackers.
+
+Escort a valuable shipment of atomic energy from an atompunk power plant to a biopunk laboratory.
+
+Rescue a steam-powered automaton that was stolen from a clockpunk inventor and taken to a dieselpunk city.
+
+Help a solarpunk community defend against an invasion by a cyborg army led by a powerful atompunk general.
+
+Infiltrate a dieselpunk weapons factory and sabotage its production of clockwork-powered war machines.
+
+Investigate a series of mysterious disappearances in a nanotech-infused cyberpunk metropolis.
+
+Help a biopunk researcher recover a stolen sample of genetic material from a group of clockpunk alchemists.
+
+Retrieve a powerful artifact that has been lost in a steam-powered temple in the middle of an atompunk wasteland.
+
+Defend a solarpunk village from a rampaging horde of nanobots controlled by a rogue cyberpunk hacker.
+
+
+
+*/
